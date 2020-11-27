349506eade18aaf875ebb65fe9e433587f1e8d56
RaeAnne Hamel <hamel8982@saskpolytech.ca>
2020-11-27 11:57:07 -0600

    created code stubs for the controller will try and work on thisand mostly complete for the weekend - RH

--- php-api/wwwroot/TaskController.php
+++ php-api/wwwroot/TaskController.php
@@ -1,7 +1,38 @@
 <?php
-
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class TaskController
 {
 
+    public static function getTask(EntityManager $em, array $requestData)
+    {
+
+    }
+
+    public static function postTask(EntityManager $em, array $requestData, Task $newTask)
+    {
+
+    }
+
+    public static function putTask(EntityManager $em, array $newTaskValue, ?Task $oldTaskValueFromDB)
+    {
+
+    }
+
+    public static function deleteTask(EntityManager $em, array $requestedData, ?Task $taskToDelete)
+    {
+
+    }
+
+
+    /********************************
+     *        HELPER METHODS        *
+     ********************************/
+
+    public static function populateTask(array $requestedData, Task &$task, array &$violations = []):bool
+    {
+        return true;
+    }
 }
