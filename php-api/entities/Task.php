dfd5280a6aaaf7b685d5bdfb21a922a83edecddc
RaeAnne Hamel <hamel8982@saskpolytech.ca>
2020-11-30 13:53:23 -0600

    Working on API, realized i was missing some key features for the entity went back and fixed it. working on the delete function.. --RH

--- php-api/entities/Task.php
+++ php-api/entities/Task.php
@@ -42,16 +42,130 @@ class Task
     protected $type;
 
     /**
-     * @ORM\column (type = "string")
-     * @Assert\NotBlank(normalizer="trim", message="Task Type is required")
+     * @ORM\column (type = "string" nullable=true)
      * @Assert\choice(choices=Task::URGENCY_TYPE, message="Choose a valid urgency type")
      */
     protected $urgency;
 
     /**
-     * @ORM\column (type = "string")
-     * @Assert\NotBlank(normalizer="trim", message="Task Type is required")
+     * @ORM\column (type = "string" nullable=true)
      * @Assert\Length(max = 180, max = "Task Type cannot be longer the {{limit}} characters.")
      */
     protected $detail;
+
+    /***************************
+     *    GETTERS AND SETTERS  *
+     ***************************/
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @param mixed $startDate
+     */
+    public function setStartDate($startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDueDate()
+    {
+        return $this->dueDate;
+    }
+
+    /**
+     * @param mixed $dueDate
+     */
+    public function setDueDate($dueDate): void
+    {
+        $this->dueDate = $dueDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCompletedDate()
+    {
+        return $this->completedDate;
+    }
+
+    /**
+     * @param mixed $completedDate
+     */
+    public function setCompletedDate($completedDate): void
+    {
+        $this->completedDate = $completedDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrgency(): ?string
+    {
+        return $this->urgency;
+    }
+
+    /**
+     * @param string|null $urgency
+     */
+    public function setUrgency(?string $urgency): void
+    {
+        $this->urgency = $urgency;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDetail(): ?string
+    {
+        return $this->detail;
+    }
+
+    /**
+     * @param string|null $detail
+     */
+    public function setDetail(?string $detail): void
+    {
+        $this->detail = $detail;
+    }
+
+
+
+
+
+
 }
--- php-api/wwwroot/TaskController.php
+++ php-api/wwwroot/TaskController.php
@@ -8,6 +8,36 @@ class TaskController
 
     public static function getTask(EntityManager $em, array $requestData)
     {
+        /**
+         * Want the option to be able to sort through the api
+         * 1. list all the field options
+         * 2. create sort using the key word sort
+         * 3. create the order
+         * 4. create the query
+         **/
+        $fieldOptions = ['id', 'startDate', 'dueDate', 'completedDate', 'type', 'urgency', 'detail'];
+
+        //task table alias is t
+        $sort = isset($_GET['sort']) && in_array($_GET['sort']) ? 't'.$_GET['sort'] : 't.id';
+
+        $order =isset($_GET['order']) && in_array($_GET['order'], ['asc', 'desc']) ?
+                    strtoupper($_GET['order']) : 'ASC';
+
+        $qb = $em ->createQueryBuilder();
+
+        $qb -> select('t')
+            -> from('Task', 't')
+            -> orderBy($sort, $order);
+
+        $taskArray = $qb ->getQuery()
+                         -> getArrayResult();
+
+        if(empty($taskArray))
+        {
+            http_response_code(404); // response code of item not found
+        }
+
+        return $taskArray;
 
     }
 
@@ -23,7 +53,17 @@ class TaskController
 
     public static function deleteTask(EntityManager $em, array $requestedData, ?Task $taskToDelete)
     {
+        $encodeResult = ' ';
 
+        if(is_null($taskToDelete))
+        {
+            http_response_code(404);
+            $encodeResult = $requestedData;
+        }
+        else
+        {
+            if($requestedData['type'] == $taskToDelete -> getType() &&)
+        }
     }
 
 
